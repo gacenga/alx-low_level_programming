fa18d827fe17a032feaff6548e25577b65afb9bd
gacenga <jimwillymurigu@gmail.com>
2023-10-05 12:00:59 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* This file is called automatically when program is executed
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
