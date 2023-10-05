f4637301fdd7b143b8cc929f5d69c97d52238c38
gacenga <jimwillymurigu@gmail.com>
2023-10-05 12:48:54 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* This is called automatically when the program is executed
+*
+* Return: 0 Always (Success)
+*/
+int main(void)
+{
+printf("size of char: %zu byte(s)\n", sizeof(char));
+printf("size of int: %zu byte(s)\n", sizeof(int));
+printf("size of long: %zu byte(s)\n", sizeof(long));
+printf("size of long long: %zu byte(s)\n", sizeof(long long));
+printf("size of short: %zu byte(s)\n", sizeof(short));
+printf("size of double: %zu byte(s)\n", sizeof(double));
+printf("size of long double: %zu byte(s)\n", sizeof(long double));
+printf("size of float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
