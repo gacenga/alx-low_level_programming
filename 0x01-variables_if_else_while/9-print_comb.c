6765240d4b9d6462c21ff8a42ec7ec0fa8aaf590
gacenga <jimwillymurigu@gmail.com>
2023-10-06 15:55:03 -0700

    single didgit combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single-digit numbers
+*
+* Return: 0 Always (Success)
+*/
+int main(void)
+{
+int myNumber;
+for (myNumber = 0; myNumber <= 99; myNumber++)
+{
+putchar(myNumber);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
