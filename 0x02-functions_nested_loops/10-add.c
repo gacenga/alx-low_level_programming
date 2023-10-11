cb010367ef19657431fc6ac93522e63f1c369c57
gacenga <jimwillymurigu@gmail.com>
2023-10-11 13:08:11 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* main - adds two integers and returns the result
+*
+* Return: 0 Always
+*/
+int main(void)
+{
+int add(int x, int y);
+return (0);
+}
+/**
+* add - adds two integers and returns the result
+*
+* @x:integer to add
+* @y:integer to add
+*
+* Return: result
+*/
+int add(int x, int y)
+{
+int result;
+result = x + y;
+return (result);
+}
