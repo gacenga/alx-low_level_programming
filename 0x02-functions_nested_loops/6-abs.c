984177a9dcfd3154a0462c008976f062d0f9a9f2
gacenga <jimwillymurigu@gmail.com>
2023-10-11 10:23:30 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* main - checks for alphabet character
+*
+* Return: 0 Always
+*/
+int main(void)
+{
+int _abs(int s);
+return (0);
+}
+/**
+* _abs - checks for alphabet character
+*
+* @s:character being checked
+*
+* Return: 0 Always
+*/
+int _abs(int s)
+{
+if (s >= 0)
+{
+_putchar(s);
+}
+else if (s < 0)
+{
+_putchar(-s);
+}
+return (0);
+}
