15ea373147fa0b3d681f6223858eedaa39ff3468
gacenga <jimwillymurigu@gmail.com>
2023-10-11 07:51:59 -0700

    4-isalpha.c-checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* main - checks for alphabet character
+* Return: 0 Always (Success)
+*/
+int main(void)
+{
+int _isalpha(int c);
+return (0);
+}
+/**
+* _isalpha - checks for alphabet character
+*
+* Parameters:
+* @c: character to be checked
+*
+* Return: 1 for alphabet charecter 0 for everything else
+*/
+int _isalpha(int c)
+{
+if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
