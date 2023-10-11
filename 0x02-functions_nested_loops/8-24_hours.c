0c496dcc9b1befeced85dd26cace38034b54b91b
gacenga <jimwillymurigu@gmail.com>
2023-10-11 12:19:33 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+* main - prints minutes of the day
+* Return: 0 Always (Success)
+*/
+int main(void)
+{
+void jack_bauer(void);
+return (0);
+}
+/**
+* jack_bauer - prints minutes of the day
+*
+* Return: 0 Always
+*/
+void jack_bauer(void)
+{
+int a = '0';
+int b = '0';
+int c = '0';
+int d = '0';
+while (a <= '2')
+{
+while ((b <= '3' && a == '2') || (b <= '9' && a < '2'))
+{
+while (c <= '5')
+{
+while (d <= '9')
+{
+_putchar(a);
+_putchar(b);
+_putchar(':');
+_putchar(c);
+_putchar(d);
+}
+}
+}
+}
+}
