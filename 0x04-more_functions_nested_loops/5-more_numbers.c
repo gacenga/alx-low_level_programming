bc828f50790f46ed371caa81e2879e68a4b83fe2
gacenga <jimwillymurigu@gmail.com>
2023-10-12 08:20:08 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,13 @@ void more_numbers(void)
 {
 char c = '0';
 char x = '0';
-char y;
 while (x <= '9')
 {
 while (c <= 14)
 {
-y = c % 10;
 if (c > 9)
 _putchar('1');
-_putchar(y + '0');
+_putchar(c % 10 + '0');
 c++;
 }
 c = '0';
