cd9b4a87a2d22131d412f84cf5df4049b60bb092
gacenga <jimwillymurigu@gmail.com>
2023-10-12 11:45:03 -0700

    corrections

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,22 @@ while (n <= 100)
 {
 if (n % 3 == 0 && n % 5 != 0)
 {
-printf("Fizz");
+printf("Fizz ");
 }
 else if (n % 3 != 0 && n % 5 == 0)
 {
-printf("Buzz");
+printf("Buzz ");
 }
 else if (n % 3 == 0 && n % 5 == 0)
 {
-printf("FizzBuzz");
+printf("FizzBuzz ");
 }
 else
 {
-printf("%d", n);
+printf("%d ", n);
 }
-putchar(' ');
 n++;
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
