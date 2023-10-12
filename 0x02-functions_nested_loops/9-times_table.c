c2899b3d6c75baa3313587a83741eb2e44830a9a
gacenga_ <122737849+gacenga@users.noreply.github.com>
2023-10-12 12:44:05 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,30 @@ while (a <= 9)
 while (b <= 9)
 {
 int c = a * b;
+if ((c == 0 || c < 10) && b != 9)
+{
 _putchar(c + '0');
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+}
+else if ((c == 0 || c < 10) && b == 9)
+{
+_putchar(c + '0');
+}
+if (c >= 10 && b != 9)
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+else if (c >= 10 && b == 9)
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
 b++;
 }
 _putchar('\n');
