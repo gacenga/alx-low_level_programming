f5a760fa037873e0facfca75201589dede51fa47
gacenga <jimwillymurigu@gmail.com>
2023-10-17 13:40:59 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* puts2 - prints every other character of a string
+*
+* @str:input
+*
+* Return:void
+*/
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+if (*str % 2 == 0)
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
