0e6b4e03a68246298a7d785279097b59a6c1f119
gacenga <jimwillymurigu@gmail.com>
2023-10-28 22:55:40 -0700

    task 0 and 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - prints its name
+*
+* @argc:argument
+* @argv:argument
+* Return:0 Always
+*/
+int main(int argc __attribute__((unused)), char *argv[])		
+{
+printf("%s\n", *argv);
+return (0);		
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - prints the number of arguments
+*
+* @argv:argument
+* @argc:argument
+* Return:0 Always
+*/
+int main(int argc, char *argv[])		
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);		
+}
