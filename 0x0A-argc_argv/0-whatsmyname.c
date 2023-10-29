6431b6fcf5f6cb519ded4a55058dcef847c2b09f
gacenga <jimwillymurigu@gmail.com>
2023-10-28 22:59:49 -0700

    task 0 and 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
 * @argv:argument
 * Return:0 Always
 */
-int main(int argc __attribute__((unused)), char *argv[])		
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", *argv);
-return (0);		
+return (0);
 }
