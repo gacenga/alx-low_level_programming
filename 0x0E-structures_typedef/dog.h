187810e11a2e4f7c417e6a1a5907323ead9bbe65
gacenga <jimwillymurigu@gmail.com>
2023-11-06 15:01:11 -0800

    dog.h-struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,20 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
-
+/**
+* struct dog - dogs information
+* @name:the name of dog
+* @age:age of the dog
+* @owner:owner of the dog
+* Description:long
+*/
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+/**
+* dog_t - typedef for struct doh
+*/
+typedef struct dog dog_t;
 #endif
