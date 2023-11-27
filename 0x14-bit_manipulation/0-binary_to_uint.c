349a7a3722a4ee588b8456b37ee59c88337259ca
gacenga <jimwillymurigu@gmail.com>
2023-11-26 20:03:28 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,13 +5,13 @@
 * binary_to_uint - converts a binary number to an unsigned int
 *
 * @b:pointer
-* Result:the converted number
+* Return:the converted number
 */
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int result = 0;
 if (b == NULL)
-return 0;
+return (0);
 while (*b != '\0')
 {
 if (*b == '0' || *b == '1')
@@ -21,8 +21,8 @@ b++;
 }
 else
 {
-return 0;
+return (0);
 }
 }
-return result;
+return (result);
 }
