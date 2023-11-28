0ed6ad3fcf48b42898affa09ea6f6ea6677ced0c
gacenga <jimwillymurigu@gmail.com>
2023-11-28 01:11:14 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 * create_file - creates a file
 *
 * @filename:pointer
-* @textcontent:pointer
+* @text_content:pointer
 * Return:1 on success
 */
 int create_file(const char *filename, char *text_content)
@@ -17,7 +17,7 @@ int fd;
 ssize_t bytes_written;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);    
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 return (-1);
 if (text_content != NULL)
